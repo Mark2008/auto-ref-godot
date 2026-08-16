ae67a91421d5eaa97c497b7c234bde58ae103bc7
malkeu <kangmark.2008@gmail.com>
2026-08-17 01:06:04 +0900

    Feature: Inherited class support

--- addons/auto-ref/auto-ref.gd
+++ addons/auto-ref/auto-ref.gd
@@ -1,77 +1,101 @@
 @tool
 extends EditorScript
 class_name AutoRef
+## `Ctrl + Shift + P` and search `Auto Ref`!!
 
-
-var node_by_type: Dictionary[StringName, Node] = {}
-var node_unique: Dictionary[StringName, bool] = {}
-var root: Node
-
+# for track changes
 var cnt := 0
-var change_scene_cnt := 0
-var last_cnt := 0
 
-#var p_temp = []
+
+# the numeric values are used for comparison
+# same means that `class_name` is identical
+# inherit means inherit idk (indirect)
+enum State {
+	#Nothing = 0,
+	Inherit = 1,
+	InheritOverlap = 2,
+	Same = 3,
+	SameOverlap = 4,
+}
+
+class Item:
+	var state: State
+	var node: Node
+
+var root: Node
+var pair: Dictionary[StringName, Item] = {}
+
+func run_root(r: Node):
+	root = r
+	pair.clear()
+	
+	walk_construct(r)
+	walk_assign(r)
+	
+	for k in pair.keys():
+		print(k)
+		print(pair[k].node)
+
+func walk_construct(n: Node):
+	var script := n.get_script() as Script
+	var state := State.Same
+	while script:
+		var type := script.get_global_name()
+		#print('a', type)
+		
+		if type in pair.keys():
+			if pair[type].state < state:
+				pair[type].state = state
+				pair[type].node = n
+			elif pair[type].state == state:
+				# change '???' to '???Overlap'
+				pair[type].state += 1
+		else:
+			# make new item
+			pair[type] = Item.new()
+			pair[type].state = state
+			pair[type].node = n
+		
+		script = script.get_base_script() # if null escape while loop
+		state = State.Inherit
+	
+	# check if it escape current scene
+	# (when child node is instanciated scene)
+	if n == root || n.owner == root:
+		for c in n.get_children(true):
+			walk_construct(c)
+
+func walk_assign(n: Node):
+	var props := n.get_property_list()
+	for p in props:
+		if (
+			p.type == Variant.Type.TYPE_OBJECT
+			and p.hint == PropertyHint.PROPERTY_HINT_NODE_TYPE
+		):
+			var type_name := p.hint_string as StringName
+			var value := n.get(p.name)
+			
+			if (
+				value == null
+				and pair.has(type_name)
+				and pair[type_name].state % 2 # not '???Overlap'
+			):
+				n.set(p.name, pair[type_name].node)
+				cnt += 1
+	
+	# check if it escape current scene
+	if n == root || n.owner == root:
+		for c in n.get_children():
+			walk_assign(c)
+
 
 func _run() -> void:
 	print("[macro] Start running AutoExportReference")
-	var roots := EditorInterface.get_open_scene_roots()
-	for r in roots:
-		last_cnt = cnt
-		run_root(r)
-		if last_cnt != cnt:
-			change_scene_cnt += 1
+	var root := EditorInterface.get_edited_scene_root()
+	run_root(root)
 		
 	print("[macro] End running AutoExportReference")
-	if change_scene_cnt == 0:
+	if cnt == 0:
 		print('[macro] Nothing changed')
 	else:
-		print('[macro] ', 
-			change_scene_cnt, ' scenes changed, ',
-			cnt, ' properties changed'
-		)
-	
-func run_root(r: Node):
-	root = r
-	node_by_type.clear()
-	node_unique.clear()
-	walk(r)
-	walk2(r)
-
-func walk(n: Node):
-	var script := n.get_script() as Script
-	if script:
-		var type_name := script.get_global_name()
-		if type_name in node_unique.keys():
-			node_unique[type_name] = false
-		else:
-			node_unique[type_name] = true
-			node_by_type[type_name] = n
-	
-	if n == root || n.owner == root:
-		for c in n.get_children(true):
-			walk(c)
-
-func walk2(n: Node):
-	#p_temp.append(n.name)
-	
-	var props := n.get_property_list()
-	for p in props:
-		if p.type == Variant.Type.TYPE_OBJECT \
-				and p.hint == PropertyHint.PROPERTY_HINT_NODE_TYPE:
-			var type_name := p.hint_string as StringName
-			var prop = n.get(p.name)
-			
-			if prop == null \
-					and node_by_type.has(type_name) \
-					and node_unique[type_name]:
-				n.set(p.name, node_by_type[type_name])
-				cnt += 1
-				#print('updated')
-				#print(p_temp)
-	
-	if n == root || n.owner == root:
-		for c in n.get_children():
-			walk2(c)
-		
-	#p_temp.pop_back()
+		print('[macro ', cnt, ' properties changed')
